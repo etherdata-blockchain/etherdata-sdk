a860b4729ec9a1c538e9bd16492476f520c57199
sirily11 <sirily1997@gmail.com>
2021-12-06 00:34:32 +0800

    update main

--- sdk-dist/kotlin/src/main/kotlin/Main.kt
+++ sdk-dist/kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 data class Post(val title: String)
 
 suspend fun main() {
-    val url = "https://debug.etdchain.net/hdkEtd@Themoon"
+    val url = System.getenv("url")
     val json_rpc = Json_rpc_methods(url = url)
     val number = json_rpc.gasPrice()
     println(number)
