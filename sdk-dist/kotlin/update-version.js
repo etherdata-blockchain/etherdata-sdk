a63d4e0020ac41bd3c7b4f9df87533b96a166022
sirily11 <sirily1997@gmail.com>
2021-12-08 23:06:40 +0800

    update update version script

--- sdk-dist/kotlin/update-version.js
+++ sdk-dist/kotlin/update-version.js
@@ -1,12 +1,16 @@
 module.exports.readVersion = function (contents) {
   const line = contents.split("\n").find((l) => l.includes("version ="));
   const items = line.split("=");
-  return items[1];
+  return items[1].replace("v", "");
 };
 
 module.exports.writeVersion = function (contents, version) {
   const lines = contents.split("\n");
   const index = lines.findIndex((l) => l.includes("version ="));
   lines[index] = `version = 'v${version}'`;
-  return lines.join("\r\n");
+  let newContent = "";
+  for (let line of lines) {
+    newContent += line + "\n";
+  }
+  return newContent;
 };
