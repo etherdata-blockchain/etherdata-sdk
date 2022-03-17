77b29baafe999f783671e9517c2ddaf6d46dbd2c
sirily11 <sirily1997@gmail.com>
2022-03-17 13:20:19 +0800

    fix: Python Repo URL

--- sdk-dist/python/setup.py
+++ sdk-dist/python/setup.py
@@ -1,14 +1,14 @@
 import setuptools
 long_description = "# Etherdata BlockChain Python SDK"
 setuptools.setup(
-    name="Etherdata Python SDK",
+    name="etherdata-sdk-python",
     version="v3.0.2",
     author="etherdata-blockchain",
     author_email="etherdata-blockchain@etd.com",
     description="SDK for etherdata-blockchain",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/etherdata-blockchain/etherdata-sdk",
     project_urls={
         "Bug Tracker": "https://github.com/etherdata-blockchain/etherdata-sdk",
     },
