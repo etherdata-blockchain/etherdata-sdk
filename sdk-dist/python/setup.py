46dd67528e10e6d1233798a0abbb98ffaf8cd029
github action <github-action.github.com>
2022-04-18 08:13:05 +0000

    Release 4.1.0

--- sdk-dist/python/setup.py
+++ sdk-dist/python/setup.py
@@ -5,7 +5,7 @@ Please refer more info on our GitHub
 """
 setuptools.setup(
     name="etherdata-sdk-python",
-    version="4.0.3",
+    version="4.1.0",
     author="etherdata-blockchain",
     author_email="etherdata-blockchain@etd.com",
     description="SDK for etherdata-blockchain",
