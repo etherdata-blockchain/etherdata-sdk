d4cf2b55e3325db8d0a6bce21710638bd6238b42
sirily11 <sirily1997@gmail.com>
2022-04-24 13:14:58 +0800

    feat: add custom error

--- sdk-dist/typescript/packages/errors/lib/upload_error.ts
+++ sdk-dist/typescript/packages/errors/lib/upload_error.ts
@@ -1,6 +1,10 @@
 export class UploadError extends Error {
-  constructor(message: string, errorCode: number, reason: string) {
-    const outputMessage = `Error ${errorCode}: Cannot upload file due to ${reason}`;
+  constructor(reason: string, errorCode?: number) {
+    let outputMessage: string = `Cannot upload file due to ${reason}`;
+    if (errorCode) {
+      outputMessage = `Error ${errorCode}: Cannot upload file due to ${reason}`;
+    }
+
     super(outputMessage);
   }
 }
